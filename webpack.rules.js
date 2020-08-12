c223dff53db86646d70ba486e1095805a6cda2d0
Lei Nelissen <lei@codified.nl>
2020-08-12 14:59:41 +0200

    Setup build pipeline for nodegit

--- webpack.rules.js
+++ webpack.rules.js
@@ -5,16 +5,16 @@ module.exports = function(configFile = 'tsconfig.json') {
             test: /\.node$/,
             use: 'node-loader',
         },
-        {
-            test: /\.(m?js|node)$/,
-            parser: { amd: false },
-            use: {
-                loader: '@marshallofsound/webpack-asset-relocator-loader',
-                options: {
-                    outputAssetBase: 'native_modules',
-                },
-            },
-        },
+        // {
+        //     test: /\.(m?js|node)$/,
+        //     parser: { amd: false },
+        //     use: {
+        //         loader: '@zeit/webpack-asset-relocator-loader',
+        //         options: {
+        //             outputAssetBase: 'native_modules',
+        //         },
+        //     },
+        // },
         {
             test: /\.tsx?$/,
             exclude: /(node_modules|\.webpack)/,
