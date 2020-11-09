706aaa14276bd01cfa0cd0628e92c4bd0c73b6bf
Lei Nelissen <lei@codified.nl>
2020-11-09 16:12:35 +0100

    Remove redundant log entries

--- src/app/store/persist.ts
+++ src/app/store/persist.ts
@@ -2,12 +2,10 @@ import { Storage } from 'redux-persist';
 
 export default function ElectronStorage(): Storage {
     return {
-        getItem: (...props) => {
-            console.log('GETITEM', props, window.api.store.retrieve());
+        getItem: () => {
             return Promise.resolve(window.api.store.retrieve());
         },
         setItem: (key: string, item: string) => {
-            console.log('SETITEM', key, item);
             return Promise.resolve(window.api.store.persist(item));
         },
         removeItem: () => {
