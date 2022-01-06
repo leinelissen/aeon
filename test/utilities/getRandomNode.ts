150cb32323479366bc9ddabea65e67aa1d157b85
Lei Nelissen <lei@codified.nl>
2022-01-06 23:56:53 +0100

    Double-check that there are enough nodes

--- test/utilities/getRandomNode.ts
+++ test/utilities/getRandomNode.ts
@@ -7,6 +7,11 @@ async function getRandomNode(locator: Locator): Promise<Locator> {
     // Count the number of nodes
     const numberOfNodes = await locator.count()
 
+    // GUARD: Check that the call did not match any nodes
+    if (numberOfNodes === 0) {
+        throw new Error('Cannot get random node from locator without matches');
+    }
+
     // Then get a random index
     // NOTE: locator.nth takes a zero-indexed index, so we need to subtract 1 as
     // locator.count() is one-indexed.
